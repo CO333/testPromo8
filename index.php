ef959214a4d8ffe22923a0e7b821df88f7d602bf
CO <c.o@e-nautia.com>
2018-11-22 11:12:20 +0100

    contact et mail index

--- index.php
+++ index.php
@@ -7,5 +7,10 @@
 <body>
 	<h1>Bonjour V1</h1>
 	<p>kihmkojh</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+		Impedit blanditiis quas asperiores amet laudantium dolorem incid
+		unt iste provident facere minima quisquam, odio similique voluptat
+		es et dignissimos magnam velit repellendus consequuntur.
+	</p>
 </body>
 </html>
